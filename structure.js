cbc0d107eb2bd067be0f90120a310ce7973f13cb
Drf coin <digitalrufiya@gmail.com>
2025-04-27 23:22:39 +0500

    Create structure.js

---
+++ structure.js
@@ -0,0 +1,8 @@
+digitalrufiya-wallet/
+├── index.html
+├── package.json
+├── vite.config.js
+├── node_modules/
+├── src/
+│   ├── main.js  (you will rename this to wallet.js or script.js)
+│   └── style.css (your css)
