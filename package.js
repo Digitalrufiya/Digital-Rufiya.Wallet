f440953cf2b13447ee002f51fe72fff68b3d878f
Drf coin <digitalrufiya@gmail.com>
2025-04-27 23:00:03 +0500

    Update package.js

--- package.js
+++ package.js
@@ -1,14 +1,21 @@
 {
   "name": "drf-wallet",
   "version": "1.0.0",
-  "description": "DRF Wallet Frontend (BSC)",
+  "description": "DRF Wallet - BSC Smart Wallet Web App",
+  "author": "Your Name",
+  "license": "MIT",
+  "private": true,
   "scripts": {
     "dev": "vite",
     "build": "vite build",
     "preview": "vite preview"
   },
-  "dependencies": {},
+  "dependencies": {
+    "@walletconnect/core": "^2.8.5",
+    "@reown/walletkit": "^1.0.6",
+    "ethers": "^6.7.0"
+  },
   "devDependencies": {
-    "vite": "^5.2.0"
+    "vite": "^5.0.0"
   }
 }
