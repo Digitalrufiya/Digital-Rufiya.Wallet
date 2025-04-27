6d2ab0d6ee2c1f32102348959db70dd5946b106c
Drf coin <digitalrufiya@gmail.com>
2025-04-27 22:38:39 +0500

    Create package.js

---
+++ package.js
@@ -0,0 +1,14 @@
+{
+  "name": "drf-wallet",
+  "version": "1.0.0",
+  "description": "DRF Wallet Frontend (BSC)",
+  "scripts": {
+    "dev": "vite",
+    "build": "vite build",
+    "preview": "vite preview"
+  },
+  "dependencies": {},
+  "devDependencies": {
+    "vite": "^5.2.0"
+  }
+}
