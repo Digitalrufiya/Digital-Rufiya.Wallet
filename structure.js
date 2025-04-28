23eb67c4d54c12d02a2d59dafe9ab9ca8727a5dd
Drf coin <digitalrufiya@gmail.com>
2025-04-28 09:50:39 +0500

    Delete structure.js

--- structure.js
+++
@@ -1,8 +0,0 @@
-digitalrufiya-wallet/
-├── index.html
-├── package.json
-├── vite.config.js
-├── node_modules/
-├── src/
-│   ├── main.js  (you will rename this to wallet.js or script.js)
-│   └── style.css (your css)
