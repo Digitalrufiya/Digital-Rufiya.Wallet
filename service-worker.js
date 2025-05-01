706f945a8aeb5293d7fa40443a853ccb7d6e3e5d
Drf coin <digitalrufiya@gmail.com>
2025-05-01 14:12:40 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,6 @@
+<script src="https://cdn.jsdelivr.net/npm/ethers@5.7.2/dist/ethers.umd.min.js"></script>
+
+
 self.addEventListener('install', function(event) {
   console.log('Service worker installed.');
 });
