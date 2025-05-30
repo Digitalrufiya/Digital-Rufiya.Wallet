38cb11e8af5f846671b558a9beda439553fe8185
Drf coin <digitalrufiya@gmail.com>
2025-05-30 23:08:18 +0500

    Add files via upload

---
+++ quran.js
@@ -0,0 +1,21 @@
+
+const quran = [
+    {
+        surah: "Al-Qaari'a",
+        ayah: 2,
+        arabic: "مَا الْقَارِعَةُ",
+        english: "What is the Striking Calamity?"
+    },
+    {
+        surah: "An-Naml",
+        ayah: 1,
+        arabic: "طس ۚ تِلْكَ آيَاتُ الْقُرْآنِ وَكِتَابٍ مُبِينٍ",
+        english: "Ta, Seen. These are the verses of the Qur'an and a clear Book"
+    },
+    {
+        surah: "An-Naml",
+        ayah: 3,
+        arabic: "الَّذِينَ يُقِيمُونَ الصَّلَاةَ وَيُؤْتُونَ الزَّكَاةَ وَهُم بِالْآخِرَةِ هُمْ يُوقِنُونَ",
+        english: "Who establish prayer and give zakah, and of the Hereafter they are certain [in faith]."
+    }
+];
