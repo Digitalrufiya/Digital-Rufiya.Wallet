e60114ff263a6476be673dcb4c92cc907f801c17
Drf coin <digitalrufiya@gmail.com>
2025-06-03 00:26:27 +0500

    Create cidList.js

---
+++ cidList.js
@@ -0,0 +1,13 @@
+// cidList.js
+const approvedCIDs = [
+  {
+    cid: "QmSeWPJehjHMWYHCmoK6m1B9uLFqFJeHUMHhd2KC6mWm9s",
+    description: "My first post!",
+    date: "2025-06-02"
+  },
+  {
+    cid: "QmExample123456789...",
+    description: "Another awesome post 😎",
+    date: "2025-06-01"
+  }
+];
