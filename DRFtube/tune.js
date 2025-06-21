59affbc79441e525e1485abdf38b7ae30c72951d
Drf coin <digitalrufiya@gmail.com>
2025-06-21 17:34:26 +0500

    Create tune.js

---
+++ DRFtube/tune.js
@@ -0,0 +1,15 @@
+{
+  "name": "DRFTube",
+  "short_name": "DRFTube",
+  "start_url": "watch.html",
+  "display": "standalone",
+  "background_color": "#111",
+  "theme_color": "#0a84ff",
+  "icons": [
+    {
+      "src": "logo.png",
+      "type": "image/png",
+      "sizes": "192x192"
+    }
+  ]
+}
