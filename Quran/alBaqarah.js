6ce7c237854ac8470b33cc2893c0951582794b13
Drf coin <digitalrufiya@gmail.com>
2025-07-05 15:03:34 +0500

    Update and rename quran_baqarah.js to alBaqarah.js

---
+++ Quran/alBaqarah.js
@@ -0,0 +1,7 @@
+const alBaqarah = [
+  { verse: 1, surah: "Al-Baqarah", arabic: "الم", english: "Alif, Lam, Meem." },
+  { verse: 2, surah: "Al-Baqarah", arabic: "ذَٰلِكَ الْكِتَابُ لَا رَيْبَ ۛ فِيهِ ۛ هُدًى لِّلْمُتَّقِينَ", english: "This is the Book about which there is no doubt, a guidance for those conscious of Allah." },
+  { verse: 3, surah: "Al-Baqarah", arabic: "الَّذِينَ يُؤْمِنُونَ بِالْغَيْبِ وَيُقِيمُونَ الصَّلَاةَ وَمِمَّا رَزَقْنَاهُمْ يُنفِقُونَ", english: "Who believe in the unseen, establish prayer, and spend out of what We have provided for them." },
+  { verse: 4, surah: "Al-Baqarah", arabic: "وَالَّذِينَ يُؤْمِنُونَ بِمَا أُنزِلَ إِلَيْكَ وَمَا أُنزِلَ مِن قَبْلِكَ وَبِالْآخِرَةِ هُمْ يُوقِنُونَ", english: "And who believe in what has been revealed to you and what was revealed before you, and of the Hereafter they are certain." },
+  { verse: 5, surah: "Al-Baqarah", arabic: "أُو۟لَـٰٓئِكَ عَلَىٰ هُدًۭى مِّن رَّبِّهِمْ ۖ وَأُو۟لَـٰٓئِكَ هُمُ ٱلْمُفْلِحُونَ", english: "It is they who are upon guidance from their Lord, and it is they who will be successful." }
+];
