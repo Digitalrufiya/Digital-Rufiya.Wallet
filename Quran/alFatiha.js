36216597e9704c4eae86ac47da27aa72782ca22b
Drf coin <digitalrufiya@gmail.com>
2025-07-05 11:57:23 +0500

    Update and rename quran1.js to alFatiha.js

---
+++ Quran/alFatiha.js
@@ -0,0 +1,9 @@
+const alFatiha = [
+  { verse: 1, arabic: "بسم الله الرحمن الرحيم", english: "In the name of Allah, the Most Gracious, the Most Merciful." },
+  { verse: 2, arabic: "الْحَمْدُ لِلَّهِ رَبِّ الْعَالَمِينَ", english: "Praise be to Allah, Lord of the worlds." },
+  { verse: 3, arabic: "الرَّحْمَنِ الرَّحِيمِ", english: "The Most Gracious, the Most Merciful." },
+  { verse: 4, arabic: "مَالِكِ يَوْمِ الدِّينِ", english: "Master of the Day of Judgment." },
+  { verse: 5, arabic: "إِيَّاكَ نَعْبُدُ وَإِيَّاكَ نَسْتَعِينُ", english: "You alone we worship, and You alone we ask for help." },
+  { verse: 6, arabic: "اهْدِنَا الصِّرَاطَ الْمُسْتَقِيمَ", english: "Guide us on the Straight Path," },
+  { verse: 7, arabic: "صِرَاطَ الَّذِينَ أَنْعَمْتَ عَلَيْهِمْ...", english: "The path of those who have received Your grace..." },
+];
